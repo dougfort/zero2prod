00e900ee9fe87157a6d344615da17781332cf2cd
dougfort <doug.fort@gmail.com>
2020-11-08 18:43:49 -0500

    up to docker

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+use zero2prod::configuration::get_configuration;
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let subscriber = get_subscriber("zero2prod".into(), "info".into());
+    init_subscriber(subscriber);
+
+    let configuration = get_configuration().expect("get_configuration failed");
+
+    let connection_string = configuration.database.connection_string();
+    let connection = PgPool::connect(&connection_string)
+        .await
+        .expect("failed to connect to postgres");
+
+    let address = format!(
+        "{}:{}",
+        configuration.application.host, configuration.application.port
+    );
+    let listener = TcpListener::bind(address)?;
+
+    let server = run(listener, connection)?;
+    server.await
 }
---
+++ src/routes/subscriptions.rs
@@ -0,0 +1,57 @@
+use actix_web::{web, HttpResponse};
+use chrono::Utc;
+use sqlx::PgPool;
+use uuid::Uuid;
+
+#[derive(serde::Deserialize)]
+pub struct SubscribeRequest {
+    email: String,
+    name: String,
+}
+
+#[tracing::instrument(
+    name = "Adding a new subscriber.",
+    skip(payload, pool),
+    fields(
+        email = %payload.email,
+        name = %payload.name
+    )
+)]
+pub async fn subscribe(
+    payload: web::Form<SubscribeRequest>,
+    pool: web::Data<PgPool>,
+) -> Result<HttpResponse, HttpResponse> {
+    insert_subscriber(&pool, &payload)
+        .await
+        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+
+    Ok(HttpResponse::Ok().finish())
+}
+
+#[tracing::instrument(
+    name = "Saving newsubscriber details in the database.",
+    skip(payload, pool)
+)]
+pub async fn insert_subscriber(
+    pool: &PgPool,
+    payload: &SubscribeRequest,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
+        r#"
+        INSERT INTO subscriptions (id, email, name, subscribed_at)
+        VALUES ($1, $2, $3, $4)
+        "#,
+        Uuid::new_v4(),
+        payload.email,
+        payload.name,
+        Utc::now()
+    )
+    .execute(pool)
+    .await
+    .map_err(|e| {
+        tracing::error!("failed to execute query: {:?}", e);
+        e
+    })?;
+
+    Ok(())
+}
