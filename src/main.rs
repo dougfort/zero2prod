937ea613912d7360152d48897e91f25a338332a4
dougfort <doug.fort@gmail.com>
2020-11-09 18:24:21 -0500

    Working Dockerfile two stages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use sqlx::PgPool;
+
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
 
 use zero2prod::configuration::get_configuration;
@@ -12,8 +13,9 @@ async fn main() -> std::io::Result<()> {
 
     let configuration = get_configuration().expect("get_configuration failed");
 
-    let connection_string = configuration.database.connection_string();
-    let connection = PgPool::connect(&connection_string)
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect(&configuration.database.connection_string())
         .await
         .expect("failed to connect to postgres");
 
@@ -23,6 +25,6 @@ async fn main() -> std::io::Result<()> {
     );
     let listener = TcpListener::bind(address)?;
 
-    let server = run(listener, connection)?;
+    let server = run(listener, connection_pool)?;
     server.await
 }
